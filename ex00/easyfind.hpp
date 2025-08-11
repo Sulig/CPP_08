c0cfeef656542bbf6982fe1a28d9cf06d20cfac7
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2025-08-11 17:37:58 +0200

    easy peasy lemon squeezy

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -5,25 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/30 16:26:20 by sadoming          #+#    #+#             */
-/*   Updated: 2025/07/30 16:27:53 by sadoming         ###   ########.fr       */
+/*   Created: 2025/07/17 12:22:54 by sadoming          #+#    #+#             */
+/*   Updated: 2025/08/11 17:31:12 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-#include <algorithm>
-#include <iterator>
-#include <vector>
+# include <algorithm>
+# include <iterator>
+# include <iostream>
+# include <stdexcept>
 
-#include <array>
-#include <iostream>
-
-template <typename T>
-typename T::iterator easyfind(T &container, int to_find)
+template<typename T>
+/**
+ * Only works with containers with iterators, like:
+ *
+ *	- std::vector
+ *
+ *	- std::list
+ *
+ *	- std::deque
+ *
+ *	- std::set
+ *
+ *	- std::map
+ *
+ * -
+ * @brief Searches for an integer in a container and returns an iterator to it.
+ * If the integer is not found, it throws a runtime error.
+ * @param arr The container to search in.
+ * @param to_find The integer to search for.
+ * @return An iterator to the found integer.
+ * @throws std::runtime_error if the integer is not found in the container.
+ ****
+ ****
+ **		- Note -> the iterator don't works as an array index, it is a pointer to the element.
+ *
+ **		- To access the value, you need to dereference it with `*it`.
+ */
+typename	T::iterator easyfind(T& arr, int to_find)
 {
-	return std::find(container.begin(), container.end(), to_find);
+	typename T::iterator it = std::find(arr.begin(), arr.end(), to_find);
+
+	if (it == arr.end())
+		throw std::runtime_error("Element not found");
+	return it;
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,8 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/30 16:17:29 by sadoming          #+#    #+#             */
-/*   Updated: 2025/07/30 16:17:30 by sadoming         ###   ########.fr       */
+/*   Created: 2025/07/17 12:22:41 by sadoming          #+#    #+#             */
+/*   Updated: 2025/08/11 17:14:13 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+// Containers
+#include <vector>
+#include <list>
+
+int main()
+{
+	// Test with a vector
+	std::vector<int> arr_i;
+	arr_i.push_back(5);
+	arr_i.push_back(7);
+	arr_i.push_back(3);
+
+	try {
+		std::cout << *easyfind(arr_i, 7) << std::endl; // Expected -> 7
+		std::cout << *easyfind(arr_i, 10) << std::endl; // Expected -> Exception
+	} catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl; // Expected -> Element not found
+	}
+	//** */
+
+	// Test with a list
+	//** */
+	std::list<int> arr_l;
+	arr_l.push_back(5);
+	arr_l.push_back(7);
+	arr_l.push_back(3);
+
+	try {
+		std::cout << *easyfind(arr_l, 3) << std::endl; // Expected -> 3
+		std::cout << *easyfind(arr_l, 10) << std::endl; // Expected -> Exception
+	} catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl; // Expected -> Element not found
+	}
+	//********************** */
+	return (0);
+}
