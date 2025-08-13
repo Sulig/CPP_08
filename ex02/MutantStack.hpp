44b13abfac9f52987e11bd6bf1768e25756a9e27
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2025-08-13 19:15:15 +0200

    Excellent, truly exelent.. now your creation will now ...

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,51 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 17:32:39 by sadoming          #+#    #+#             */
-/*   Updated: 2025/08/12 17:32:40 by sadoming         ###   ########.fr       */
+/*   Updated: 2025/08/13 19:04:43 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		MutantStack() : std::stack<T>() {}
+		MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+		~MutantStack() {}
+
+		MutantStack &operator=(const MutantStack &other)
+		{
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return *this;
+		}
+
+		// Iterators
+		/* Using member `c` of stack to recreate iterators function */
+		iterator begin() { return this->c.begin(); }
+		const_iterator begin() const { return this->c.begin(); }
+
+		iterator end() { return this->c.end(); }
+		const_iterator end() const { return this->c.end(); }
+
+		// Reverse iterators
+		reverse_iterator rbegin() { return this->c.rbegin(); }
+		const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+
+		reverse_iterator rend() { return this->c.rend(); }
+		const_reverse_iterator rend() const { return this->c.rend(); }
+};
+
+#endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,71 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:45:00 by sadoming          #+#    #+#             */
-/*   Updated: 2025/08/12 16:45:01 by sadoming         ###   ########.fr       */
+/*   Updated: 2025/08/13 19:10:46 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+void test()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+}
+/**///* */
+
+void myTest()
+{
+	MutantStack<int> mstack;
+	mstack.push(1);
+	mstack.push(2);
+	mstack.push(3);
+	mstack.push(4);
+	mstack.push(5);
+
+	MutantStack<int> mstack2(mstack); // Copy constructor
+	mstack2.push(6); // Modify the copy
+
+	std::cout << "Iterating through MutantStack:" << std::endl;
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+
+	std::cout << "Reverse Iterating through MutantStack:" << std::endl;
+	for (MutantStack<int>::reverse_iterator rit = mstack.rbegin(); rit != mstack.rend(); ++rit)
+		std::cout << *rit << " ";
+	std::cout << std::endl;
+
+	std::cout << "Size of mstack: " << mstack.size() << std::endl;
+	std::cout << "Size of mstack2: " << mstack2.size() << std::endl;
+	std::cout << "Top element of mstack: " << mstack.top() << std::endl;
+	std::cout << "Top element of mstack2: " << mstack2.top() << std::endl;
+}
+
+int main()
+{
+	std::cout << "Testing MutantStack with subject test function ->" << std::endl;
+	test();
+	std::cout << "Testing MutantStack with custom test function ->" << std::endl;
+	myTest();
+	return 0;
+}
